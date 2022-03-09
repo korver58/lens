c1edaa4170d2ba05e2d36d577e197110c42f0916
korver58 <ktokyoforest@yahoo.co.jp>
2022-03-10 00:33:42 +0900

    update

--- make_map.py
+++ make_map.py
@@ -1,3 +1,4 @@
+from msilib.schema import RemoveFile
 import numpy as np
 import cv2
 import math
@@ -13,7 +14,7 @@ def make_map_resize(src_w, src_h, scale=2.0):
 
     return map[:,:,0], map[:,:,1]
 
-def make_map_inv(map_src):
+def make_map_inv(map_src, scale):
     h, w = map_src[0].shape
     map_nop = np.zeros((h, w, 2), np.float32)
     for y in range(h):
@@ -23,13 +24,14 @@ def make_map_inv(map_src):
 
     map = cv2.remap(map_nop, map_src[0], map_src[1], cv2.INTER_NEAREST)
 
-    map_inv = np.zeros((h, w, 2), np.float32)
+    map_inv = np.zeros((h//scale, w//scale, 2), np.float32)
     for y in range(h):
         for x in range(w):
             fx, ix = math.modf(map[y][x][0])
-            fy, iy = math.modf(map[y][x][1])          
-            map_inv[int(iy)][int(ix)][0] =  x
-            map_inv[int(iy)][int(ix)][1] =  y
+            fy, iy = math.modf(map[y][x][1])
+            if ix < w//scale and iy < h//scale:
+               map_inv[int(iy)][int(ix)][0] =  x
+               map_inv[int(iy)][int(ix)][1] =  y
  
     return map_inv[:,:,0], map_inv[:,:,1]
 
@@ -46,14 +48,16 @@ def test():
                         [0.,    0., 1. ]])
     dist_coef = np.array([3.0, 4.0, 1.0, 0, 0])
 
-    new_cam_mat, roi_size = cv2.getOptimalNewCameraMatrix(cam_mat, dist_coef, (w, h), 1)
-    map_undis = cv2.initUndistortRectifyMap(cam_mat, dist_coef, None, new_cam_mat, (w, h), cv2.CV_32FC1)
+    scale = 2
+    new_cam_mat, roi_size = cv2.getOptimalNewCameraMatrix(cam_mat, dist_coef, (w, h), 1, (w*scale, h*scale))
+    map_undis = cv2.initUndistortRectifyMap(cam_mat, dist_coef, None, new_cam_mat, (w*scale, h*scale), cv2.CV_32FC1)
     undist = cv2.remap(src, map_undis[0], map_undis[1], cv2.INTER_CUBIC)
     cv2.imshow("undis", undist)
 
-    map_inv = make_map_inv(map_undis)
+    map_inv = make_map_inv(map_undis, scale)
     redist = cv2.remap(undist, map_inv[0], map_inv[1], cv2.INTER_CUBIC)
     cv2.imshow("redist", redist)
+    cv2.imwrite("out.png", redist)
 
     cv2.waitKey(0)
 
